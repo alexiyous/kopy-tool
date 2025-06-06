9ce820a14872f8a4a455c566f10b923cd9ed9bf4
Alexius Andrianno Alfa Renanta <alexiusrenanta@gmail.com>
2025-06-06 21:19:59 +0700

    configurations for publishing

--- copytool/build.gradle.kts
+++ copytool/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.android)
+    id("maven-publish")
 }
 
 android {
@@ -30,6 +31,13 @@ android {
     kotlinOptions {
         jvmTarget = "11"
     }
+
+    publishing {
+        singleVariant("release") {
+            withSourcesJar()
+            withJavadocJar()
+        }
+    }
 }
 
 dependencies {
@@ -42,4 +50,45 @@ dependencies {
     androidTestImplementation(libs.androidx.espresso.core)
 
     implementation("io.github.java-diff-utils:java-diff-utils:4.9")
+}
+
+afterEvaluate {
+    publishing {
+        publications {
+            create<MavenPublication>("release") {
+                from(components["release"])
+
+                groupId = "com.github.alexiyous"
+                artifactId = "kopytool"
+                version = "1.0.0"
+
+                pom {
+                    name.set("KopyTool")
+                    description.set("Android Copy Tool Library")
+                    url.set("https://github.com/alexiyous/kopy-tool")
+
+                    licenses {
+                        license {
+                            name.set("Apache License 2.0")
+                            url.set("https://www.apache.org/licenses/LICENSE-2.0")
+                        }
+                    }
+
+                    developers {
+                        developer {
+                            id.set("alexiyous")
+                            name.set("Alexius Andrianno Alfa Renanta")
+                            email.set("alexiusrenanta@gmail.com")
+                        }
+                    }
+
+                    scm {
+                        connection.set("scm:git:github.com/alexiyous/kopy-tool.git")
+                        developerConnection.set("scm:git:ssh://github.com/alexiyous/kopy-tool.git")
+                        url.set("https://github.com/alexiyous/kopy-tool/tree/main")
+                    }
+                }
+            }
+        }
+    }
 }
